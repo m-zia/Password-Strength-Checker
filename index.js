7cbd63f74889debb7acc9fd0cfc49a2dade28cf7
M Zia <murtaza_zia@hotmail.co.uk>
2023-08-24 23:30:02 +0100

    added JS logic for changing the color of the progress bar

---
+++ index.js
@@ -0,0 +1,29 @@
+let password = document.getElementById("password");
+
+password.addEventListener('keyup', function() {
+    let pwd = password.value;
+
+    // Reset if password length is zero
+    if (pwd.length === 0) {
+    document.getElementById("progresslabel").innerHTML = "";
+    document.getElementById("progress-bar").style.width = "0%";
+    return;
+    }
+
+    let progress = pwd.length * 5;
+    progress = Math.min(progress, 100); // limit progress to 100 so it doesn't overflow
+
+    let progressBar = document.getElementById("progress-bar");
+
+    // Calculate progress bar color based on password length
+    if (pwd.length < 8) {
+    progressBar.style.background = "red";
+    } else if (pwd.length >= 8 && pwd.length < 12) {
+    progressBar.style.background = "orange"; 
+    } else {
+    progressBar.style.background = "green"; 
+    }
+
+    
+
+});
